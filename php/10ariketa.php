60c44161392926f6509919598aa17f7b7229a01a
Josue <j_basterrecheaazk@fpzornotzalh.eus>
2026-09-14 10:17:55 +0200

    Komponduta

--- php/10ariketa.php
+++ php/10ariketa.php
@@ -2,6 +2,6 @@
 date_default_timezone_set('Europe/Madrid');
 $now = new DateTime();
 $future = new DateTime('2026-12-31 23:59:59');
-$interval = $now->date_diff($future);
+$interval = $now->diff($future);
 echo $interval->format('%R%a days, %H hours, %I minutes, %S seconds');
 ?>
