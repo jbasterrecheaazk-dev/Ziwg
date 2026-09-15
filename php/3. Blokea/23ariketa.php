7c23c9badec4d5acc1fa7a2d70390fa943357424
Josue <j_basterrecheaazk@fpzornotzalh.eus>
2026-09-15 11:30:19 +0200

    Commit

---
+++ php/3. Blokea/23ariketa.php
@@ -0,0 +1,5 @@
+<?php
+if (isset($_GET["number1"]) && isset($_GET["number2"])) {
+    
+}
+?>
---
+++ php/3. Blokea/24ariketa.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_GET["bi"])) {
+    $bikoiti = $_GET["bi"];
+    $top = rand(1, 99);
+    for ($number=0; $number < $top; $number++) { 
+        if ($bikoiti && $number % 2 == 0) {
+            echo $number . "<br>";
+        } elseif (!$bikoiti && $number % 2 != 0) {
+            echo $number . "<br>";
+        }
+    }
+} else {
+    echo "Ez definituta.";    
+}
+?>
