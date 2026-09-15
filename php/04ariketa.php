478d6bad00b57f5c23fa57bd0468e2b271274a44
Josue <j_basterrecheaazk@fpzornotzalh.eus>
2026-09-15 08:48:17 +0200

    Commit

--- php/04ariketa.php
+++ php/04ariketa.php
@@ -1,14 +1,14 @@
 <?php
-$n1 = 1;
-$n2 = 2;
+$n1 = 2;
+$n2 = 1;
 $n3 = 3;
 if ($n1 > $n2 && $n2 > $n3 && $n1 > $n3) {
 	echo $n1 . $n2 . $n3;
 } elseif ($n1 < $n2 && $n2 > $n3 && $n1 > $n3) {
 	echo $n2 . $n1 . $n3;
-} elseif ($n1 < $n2 && $n2 < $n3 && $n1 > $n3) {
-	echo $n1 . $n2 . $n3;
 } elseif ($n1 < $n2 && $n2 < $n3 && $n1 < $n3) {
 	echo $n3 . $n2 . $n1;
+} elseif ($n1 = $n2 && $n2 = $n3 && $n1 = $n3) {
+	echo "Zenbaki berdinak dira.";
 }
 ?>
