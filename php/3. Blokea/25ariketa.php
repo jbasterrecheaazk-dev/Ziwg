8be21e18b39b2ae5589fe33779b5cbf1806eb9ed
Josue <j_basterrecheaazk@fpzornotzalh.eus>
2026-09-17 09:24:27 +0200

    Commit

--- php/3. Blokea/25ariketa.php
+++ php/3. Blokea/25ariketa.php
@@ -1,6 +1,17 @@
 <?php
-session_start();
-if (isset($_GET["N"])) {
-    $NN = (float)
+function lehena($_GET["N"]) {
+    if ($_GET["N"] <= 1) return false;
+    if ($_GET["N"] == 2) return true;
+    if ($_GET["N"] % 2 == 0) return false;
+    $top = ceil(sqrt($_GET["N"]));
+    for ($i = 3; $i <= $top; $i += 2) {
+        if ($_GET["N"] % $i == 0) return false;
+    }
+    return true;
+}
+if (lehena($_GET["N"])) {
+    echo "Zenbakia lehena da.";
+} else {
+    echo "Zenbakia ez da lehena.";
 }
 ?>
