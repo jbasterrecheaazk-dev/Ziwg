1b8b9457e579850b669bfa238e0e2fbb0195f079
Josue <j_basterrecheaazk@fpzornotzalh.eus>
2026-09-22 09:58:13 +0200

    Commit

--- php/4. Blokea/32ariketa.php
+++ php/4. Blokea/32ariketa.php
@@ -1,3 +1,15 @@
 <?php
-
+$zenbakiak = array('', '', '', '', '', '', '', '', '', '');
+$batuketa = 0;
+$contadorea = 0;
+for ($i=0; $i < count($zenbakiak); $i++) {
+    $numero = random_int(0, 99);
+    $zenbakiak[$i] = $numero;
+    echo "<br>" . $zenbakiak[$i] . "<br>";
+    $batuketa += $zenbakiak[$i];
+    $contadorea++;
+    echo "<br>" . $batuketa . "<br>";
+}
+$batezbestekoa = $batuketa / $contadorea;
+echo "<br>" . "Batezbestekoa:" . $batezbestekoa;
 ?>
