286a97cecfa72c8fea049b4c23fd6c2681b273a2
Josue <j_basterrecheaazk@fpzornotzalh.eus>
2026-09-22 09:41:34 +0200

    Commit

--- php/4. Blokea/30ariketa.php
+++ php/4. Blokea/30ariketa.php
@@ -1,10 +1,10 @@
 <?php
  $prezioak = array('24', '35', '40', '23', '35', '84', '55', '40', '23', '35');
+$kontua = 0;
                 for ($i = 0; $i < count($prezioak); $i++) {
-                    $kontua = 0;
-                    if ($kontua < $prezioak[$i]) {
+                    if ($kontua <= $prezioak[$i]) {
                         $kontua = $prezioak[$i];
-                        echo $prezioak[$i];
+                        echo "<br>" . $kontua . "<br>";
                     }
                 }
 ?>
--- php/4. Blokea/31ariketa.php
+++ php/4. Blokea/31ariketa.php
@@ -1,3 +1,16 @@
 <?php
-
+$zenbakiak = array('', '', '', '', '', '', '', '', '', '');
+for ($i=0; $i < count($zenbakiak); $i++) {
+    $numero = random_int(0, 99);
+    $zenbakiak[$i] = $numero;
+    echo "<br>" . $zenbakiak[$i] . "<br>";
+}
+$txikiena = 100;
+for ($i=0; $i < count($zenbakiak); $i++) {
+    
+    if ($zenbakiak[$i] < $txikiena) {
+        $txikiena = $zenbakiak[$i];
+    }
+}
+echo "Txikiena: " . $txikiena;
 ?>
