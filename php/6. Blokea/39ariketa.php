65487b4435ecc5696727615f974e047078677564
Josue <j_basterrecheaazk@fpzornotzalh.eus>
2026-09-24 10:27:06 +0200

    Commit

--- php/6. Blokea/39ariketa.php
+++ php/6. Blokea/39ariketa.php
@@ -1,18 +1,4 @@
 <?php
-session_start();
-
-$erabiltzailea = "admin";
-$pasahitza = "123456";
-
-$Erabiltzailea = $_POST['erabiltzailea'] ?? '';
-$Pasahitza = $_POST['pasahitza'] ?? '';
-
-if ($Erabiltzailea === $erabiltzailea && $Pasahitza === $pasahitza) {
-//  $_SESSION['erabiltzailea'] = $Erabiltzailea;
-//  header("Location: ")
-    echo $Erabiltzailea . " " . $Pasahitza;
-} else {
-    echo "Erabiltzailea edo pasahitza ez dago zuzen.";
-}
-
+echo '$Erabiltzailea =', $_POST['Erabiltzailea'], '<br>';
+echo '$Pasahitza =', $_POST['Pasahitza'], '<br>';
 ?>
